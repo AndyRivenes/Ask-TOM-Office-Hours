3fc996a45506d1ebd19caeff8b41370b638a7c66
Andy Rivenes <36783062+AndyRivenes@users.noreply.github.com>
2020-06-01 14:10:45 -0700

    Update imq-serial.sql

--- imq-serial.sql
+++ imq-serial.sql
@@ -16,7 +16,8 @@ set echo on
 
 -- Demonstrate an in-memory join query
 
-Select sum(lo_extendedprice * lo_discount) revenue
+Select /*+ MONITOR */
+       sum(lo_extendedprice * lo_discount) revenue
 From   LINEORDER l, DATE_DIM d
 Where  l.lo_orderdate = d.d_datekey
 And    l.lo_discount between 2 and 3
